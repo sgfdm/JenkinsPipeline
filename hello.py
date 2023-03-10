5c4b904d94b9ba3e5af61c7af8eca47f7e98f230
sgfdm <102822826+sgfdm@users.noreply.github.com>
2023-03-10 13:55:47 -0500

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+print "Content-Type: text/html"
+print
+print """\
+<html>
+ <head>
+  <title>CICD Pipeline</title>
+ </head>
+ <body>
+  Name: Sathusan Ganesathasan
+ </body>
+</html>
+"""
